f3ffe8330e0c3f18d274e64ded57928bd7f5a8e3
RadhaSiddeshwaraiah <RadhaSiddeshwaraiah@DESKTOP-O795BLK>
2020-09-21 10:33:26 +0530

    demo of push

---
+++ cucumber/src/main/java/practise/cucumber/App.java
@@ -0,0 +1,13 @@
+package practise.cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
